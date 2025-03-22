ac6751eacf0ae2028d75b9614b90cb53f5e1103e
Anv0l <andrei.voloshin@live.com>
2025-03-22 12:41:02 +0300

    Выполнил ДЗ № 9 Задачие 1
    Добавил Диалог при клике на фото с двумя опциями:
    - Сделать фото: запрашивается разрешение на использование камеры; если разрешение получено, отображаем картинку. Если доступ к камере запрещён, то при следующих запросах предлагается перейти в настройки и дать доступ оттуда.
    
    - Выбрать фото

--- app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -1,15 +1,58 @@
 package otus.gpb.homework.activities
 
+import android.Manifest.permission
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
+
+    private val permissionCamera =
+        registerForActivityResult(ActivityResultContracts.RequestPermission())
+        { isGranted ->
+            when {
+                isGranted -> {
+                    takePhoto(false)
+                }
+                !shouldShowRequestPermissionRationale(permission.CAMERA) -> {
+                    showRationaleDialog()
+                }
+                else -> {}
+            }
+        }
+
+    private fun showRationaleDialog() {
+        val permissionDialog = AlertDialog.Builder(this)
+        with (permissionDialog) {
+            setTitle("Доступ к камере")
+            setMessage("Разрешите доступ к камере для загрузки фото милого котика")
+            setPositiveButton("Дать доступ") {
+                _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = Uri.fromParts("package", packageName, null)
+                }
+                startActivity(intent)
+            }
+            setNegativeButton("Отмена") {
+                _, _ ->
+            }
+            show()
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +67,14 @@ class EditProfileActivity : AppCompatActivity() {
                         openSenderApp()
                         true
                     }
+
                     else -> false
                 }
             }
+        }
+
+        imageView.setOnClickListener {
+            showDialog(this)
         }
     }
 
@@ -39,6 +87,48 @@ class EditProfileActivity : AppCompatActivity() {
     }
 
     private fun openSenderApp() {
-        TODO("В качестве реализации метода отправьте неявный Intent чтобы поделиться профилем. В качестве extras передайте заполненные строки и картинку")
+        Toast.makeText(this, "test", Toast.LENGTH_LONG).show()
     }
+
+    fun showDialog(context: Context) {
+        val builder = AlertDialog.Builder(context)
+        with(builder) {
+            setTitle("Выберите действие")
+            setNegativeButton("Отмена") { _, _ ->
+            }
+            setItems(arrayOf("Сделать фото", "Выбрать фото")) { _, which ->
+                when (which) {
+                    TAKE_PHOTO -> takePhoto()
+                    CHOSE_PICTURE -> choosePicture()
+                }
+            }
+        }
+        builder.show()
+    }
+
+    private fun choosePicture() {
+
+    }
+
+    private fun takePhoto(needCheckPermission: Boolean = true) {
+        if (needCheckPermission) {
+            val isGranted =
+                ContextCompat.checkSelfPermission(this, permission.CAMERA)
+            if (isGranted == PackageManager.PERMISSION_GRANTED) {
+                imageView.setImageDrawable(getDrawable(R.drawable.cat))
+            } else {
+                permissionCamera.launch(permission.CAMERA)
+            }
+        } else {
+            imageView.setImageDrawable(getDrawable(R.drawable.cat))
+        }
+
+    }
+
+    companion object ImageViewOptions {
+        const val TAKE_PHOTO = 0
+        const val CHOSE_PICTURE = 1
+    }
+
+
 }
